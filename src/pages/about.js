628d6397895eea3459fbca708e4b87c75227ad61
hongmei <hyla526@hotmail.com>
2019-12-14 16:54:18 +0800

    Connected to Contentful

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,22 +4,21 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroudSection from "../components/Globals/BackgroundSection"
-import Info from "../components/Home/info"
 
-const IndexPage = ({ data }) => (
+const AboutPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <BackgroudSection
       img={data.img.childImageSharp.fluid}
-      title="regular joe's"
-    ></BackgroudSection>
-    <Info></Info>
+      title="about us"
+      styleClass="about-background"
+    />
   </Layout>
 )
 
 export const query = graphql`
   {
-    img: file(relativePath: { eq: "default-background.jpeg" }) {
+    img: file(relativePath: { eq: "about-background.jpeg" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
@@ -29,4 +28,4 @@ export const query = graphql`
   }
 `
 
-export default IndexPage
+export default AboutPage
